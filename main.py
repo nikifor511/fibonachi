84b58878b901ab77f7f5315fe09c8e1c52ba08ba
nikifor511 <deadnikifor@gmail.com>
2020-09-11 10:42:41 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,41 +1,47 @@
+def fibonacci(n):
+    """
+    вычисляя очередное число Фибоначчи через доп переменную,
+    проверяем, если четное - кладем в res
+    """
+    assert n > 0, "n must be > 0"
 
-def fibonachi(n):
-    prev_val = 0                                    # предыдущее (первое) значение 0 - четное -> выводим
-    print(prev_val)
-    curr_val = 1                                    # текущее значение
-    count_even = 1                                  # кол-во найденных четных чисел Ф
+    fib_prev = 0
+    fib_curr = 1
+    res = [0]  # первое четное число Фибоначчи
 
-    while count_even < n:                           # пока кол-во найденнх четных чисел Ф < заданного
-        curr_val_tmp = prev_val + curr_val
-        prev_val = curr_val
-        curr_val = curr_val_tmp                     # вычисляем через доп переменную следующее число Ф
-        if curr_val % 2 == 0:                       # если оно четное -> выводим его и увеличиваем счетчик
-            count_even = count_even + 1
-            print(curr_val)
+    while len(res) < n:
+            fib_curr1 = fib_prev + fib_curr
+            fib_prev = fib_curr
+            fib_curr = fib_curr1
+            if fib_curr % 2 == 0:
+                res.append(fib_curr)
+
+    return res
 
 
-def fibonachi_1(n):                                 # считаю, что каждое 3е число Ф в последовательности
-                                                    # будет четным  (1е, 4е, 7е, 10е и т.д.
-                                                    # т.к. 2 нечетных числа в сумме дают четное,
-                                                    # далее нечет + чет = нечет, далее чет + нечет = нечет и т.д.
+def even_fibonacci(n):
+    """
+    из статьи https://habr.com/ru/post/450594/
+    по теореме Люка: каждое третье число Фибоначчи — чётное,
+    а каждое чётное имеет номер кратный трём.
+    Далее воспользуемся формулой F(n+3) = 4F(n) + F(n-3)
+    """
 
-    prev_val = 0                                    # предыдущее (первое) значение 0 - четное -> выводим
-    print(prev_val)
-    curr_val = 1
-    count = 2                                       # общее кол-во найденных чисел Ф
-    count_even = 1                                  # кол-во найденных четных чисел Ф
+    assert n > 0, "n must be > 0"
 
-    while count_even < n:
-        curr_val_tmp = prev_val + curr_val
-        prev_val = curr_val
-        curr_val = curr_val_tmp                     # вычисляем через доп переменную следующее число Ф
-        count = count + 1                           # увеличиваем счетчик найденных на 1
-        if count % 3 == 1:                          # если данное число Ф по счету 1е, 4е, 7е, 10е и т.д. ->
-            count_even = count_even + 1             # выводим его и увеличиваем счетчик
-            print(curr_val)
+    res = [0]  # первое четное число Фибоначчи
+    if n >= 2:
+        res.append(2)  # второе четное число Фибоначчи
+        for i in range(2, n):
+            fib_curr = 4 * res[i-1] + res[i-2]
+            res.append(fib_curr)
+
+    return res
 
 
 if __name__ == '__main__':
-    fibonachi(10)
-    print("-----------------------------")
-    fibonachi_1(10)
+    for fib in fibonacci(3):
+        print(fib)
+    print("-------------------------------------")
+    for fib in even_fibonacci(10):
+        print(fib)
